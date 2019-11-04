f1d7805405bf9e7ee6d5a6c8f89d5565140e0e7f
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-03 19:26:34 -0700

    configures simplecov gem; changes variable name

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,5 +1,6 @@
 class GeocodeService
   attr_reader :geocode_response
+  
   def initialize(location)
     @geocode_response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json?') do |faraday|
                   faraday.params[:key] = ENV['google_api_key']
