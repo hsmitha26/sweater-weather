2965b97c1b6822807cf280559575c9c3fb19bfa4
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-03 19:32:23 -0700

    ignores /coverage and changes variable name

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,7 +1,7 @@
 class ForecastFacade
-  attr_reader :parsed_forecast_response
+  attr_reader :forecast_response
   def initialize(latitude, longitude)
-    @parsed_forecast_response = response(latitude, longitude)
+    @forecast_response = response(latitude, longitude)
   end
 
   def response(latitude, longitude)
