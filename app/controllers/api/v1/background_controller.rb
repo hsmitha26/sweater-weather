0de97f667bf4fab44e6a7fb3255f8fb8c7d8bb01
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-04 15:28:50 -0700

    establish connection to Unsplash API

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::BackgroundController < ApplicationController
   def show
-
+    connection = Faraday.get('https://api.unsplash.com/search/photos') do |faraday|
+                  faraday.params[:client_id] = ENV['unsplash_client_id']
+                  faraday.params[:query] = params[:location]
+                  faraday.params[:per_page] = 1
+                end
+    parsed_response = JSON.parse(connection.body, symbolize_names: true)
+    parsed_unsplash_data = parsed_response[:results][0][:urls]
+# binding.pry
   end
 end
