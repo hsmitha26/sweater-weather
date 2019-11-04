01d704338aff1fd508a2db7e84bafae6d76c739c
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-04 16:13:54 -0700

    Added more POROs to refactor JSON object which were using hashes earlier

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,49 +1,27 @@
 class Forecast
-  attr_reader :id, :forecast_data
+  attr_reader :id
 
   def initialize(parsed_forecast)
     @id = "0"
     @forecast_data = parsed_forecast
-    @current_weather = current_weather
     @hourly_weather = hourly_weather
     @daily_weather = daily_weather
   end
 
   def current_weather
-    {
-     time: forecast_data[:currently][:time],
-     summary: forecast_data[:currently][:summary],
-     icon: forecast_data[:currently][:icon],
-     temperature: forecast_data[:currently][:temperature],
-     humidity: forecast_data[:currently][:humidity],
-     feels_like: forecast_data[:currently][:apparentTemperature],
-     visibility: forecast_data[:currently][:visibility],
-     UV_index: forecast_data[:currently][:uvIndex],
-     high: forecast_data[:daily][:data].first[:temperatureHigh],
-     low: forecast_data[:daily][:data].first[:temperatureLow]
-    }
-  end
-
-  def hourly_weather
-    forecast_data[:daily][:data][0..7].map do |day|
-      { day: day[:time],
-        time: day[:time],
-        summary: day[:summary],
-        icon: day[:icon],
-        precipitation: day[:precipProbability],
-        high: day[:temperatureHigh],
-        low: day[:temperatureLow]
-      }
-    end
+    @current_weather ||= CurrentWeather.new(@forecast_data)
   end
 
   def daily_weather
-    forecast_data[:hourly][:data][0..11].map do |hour|
-       { time: hour[:time],
-         summary: hour[:summary],
-         temperature: hour[:temperature],
-         icon: hour[:icon]
-       }
+    @forecast_data[:daily][:data][0..7].map do |day|
+      DailyWeather.new(day)
+    end
+  end
+
+  def hourly_weather
+    binding.pry
+    @forecast_data[:hourly][:data][0..11].map do |hour|
+      HourlyWeather.new(hour)
      end
   end
 end
