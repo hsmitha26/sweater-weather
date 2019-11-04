d32fd497ba49c6e47d373e1a94b35ed5ef5e2862
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-04 10:22:53 -0700

    gets name of antipode city via Geocode reverse lookup

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -13,6 +13,17 @@ class Api::V1::AntipodeController < ApplicationController
     antipode_longitude = parse_amypode_response[:long]
 
     antipode_parsed_forecast = ForecastFacade.new(antipode_latitude, antipode_longitude).forecast_response
+    current_antipode_forecast = antipode_parsed_forecast[:currently]
+    #antipode_parsed_forecast[:currently][:summary]
+    #antipode_parsed_forecast[:currently][:temperature]
+
+    #reverse geocode lookup
+    reverse_geocode_connection = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json?') do |faraday|
+                  faraday.params[:key] = ENV['google_api_key']
+                  faraday.params[:latlng] = "#{antipode_latitude},#{antipode_longitude}"
+                end
+    parse_reverse_geocode_response = JSON.parse(reverse_geocode_connection.body, symbolize_names: true)[:results][0][:formatted_address]
+
     binding.pry
   end
 end
