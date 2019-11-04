d9678c524334099f822bad94032cb7a577410f53
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-04 11:54:29 -0700

    refactors AmypodeFacade to accept one argument - params[:location]

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,11 +1,8 @@
 class Api::V1::AntipodeController < ApplicationController
   def show
-    latitude = LocationFacade.new(params[:location]).latitude
-    longitude = LocationFacade.new(params[:location]).longitude
+    antipode_forecast = AmypodeFacade.new(params[:location]).antipode_parsed_forecast
 
-    antipode_forecast = AmypodeFacade.new(latitude, longitude).antipode_parsed_forecast
-
-    antipode_city = AmypodeFacade.new(latitude, longitude).antipode_city
+    antipode_city = AmypodeFacade.new(params[:location]).antipode_city
 
     data_prep = AntipodeForecast.new(params[:location], antipode_forecast, antipode_city)
 
--- app/facades/amypode_facade.rb
+++ app/facades/amypode_facade.rb
@@ -1,27 +1,37 @@
 class AmypodeFacade
   attr_reader :antipode_parsed_forecast, :antipode_city
 
-  def initialize(latitude, longitude)
-    @amypode_response = response(latitude, longitude)
-    @latitude = @amypode_response[:lat]
-    @longitude = @amypode_response[:long]
+  def initialize(search_city)
+    @search_city_latitude = search_city_latitude(search_city)
+    @search_city_longitude = search_city_longitude(search_city)
+    @amypode_response = response(@search_city_latitude, @search_city_longitude)
+    @antipode_latitude = @amypode_response[:lat]
+    @antipode_longitude = @amypode_response[:long]
   end
 
   def antipode_city
-    ReverseGeocodeService.new(@latitude, @longitude).parsed_response
+    ReverseGeocodeService.new(@antipode_latitude, @antipode_longitude).parsed_response
   end
 
   def antipode_parsed_forecast
-    antipode_forecast = ForecastFacade.new(@latitude, @longitude).forecast_response
+    antipode_forecast = ForecastFacade.new(@antipode_latitude, @antipode_longitude).forecast_response
     antipode_forecast[:currently]
   end
 
   def response(latitude, longitude)
-    response = AmypodeService.new(latitude, longitude).connection
+    response = AmypodeService.new(@search_city_latitude, @search_city_longitude).connection
     parse_response(response)[:data][:attributes]
   end
 
   def parse_response(response)
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def search_city_latitude(search_city)
+    LocationFacade.new(search_city).latitude
+  end
+
+  def search_city_longitude(search_city)
+    LocationFacade.new(search_city).longitude
   end
 end
