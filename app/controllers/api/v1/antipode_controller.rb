a3b42824d65816d7e60fd337902f41c3e79c3bd2
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-04 09:38:56 -0700

    fetches lat/lng for Hong Kong, Postman response has lat/lng for Jujuy, Argentina

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::AntipodeController < ApplicationController
   def show
-
+    binding.pry
+    latitude = LocationFacade.new(params[:location]).latitude
+    longitude = LocationFacade.new(params[:location]).longitude
   end
 end
