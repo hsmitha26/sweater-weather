51bc11d9fd7a68ff2e3d3f4a6a1dcf9ab9c14396
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-04 09:42:23 -0700

    adds Faraday connection to Amypode API

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::AntipodeController < ApplicationController
   def show
-    binding.pry
     latitude = LocationFacade.new(params[:location]).latitude
     longitude = LocationFacade.new(params[:location]).longitude
+
+    amypode_connection = Faraday.get('http://amypode.herokuapp.com/api/v1/antipodes?') do |faraday|
+                  faraday.headers[:api_key] = ENV['amypode_api_key']
+                  faraday.params[:lat] = latitude
+                  faraday.params[:long] = longitude
+                end
+    binding.pry
   end
 end
