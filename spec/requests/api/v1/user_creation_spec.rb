a422271611a261a90c8fc04ed15bef0da1c6d539
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 15:36:57 -0700

    adds sad path test

--- spec/requests/api/v1/user_creation_spec.rb
+++ spec/requests/api/v1/user_creation_spec.rb
@@ -9,8 +9,18 @@ describe 'Create User object' do
     }
     post "/api/v1/users", :params => params
     expect(response).to be_successful
-    
+
     parsed_response = JSON.parse(response.body, symbolize_names: true)
     expect(parsed_response).to have_key(:api_key)
+    expect(response).to have_http_status(201)
+  end
+
+  it "renders 404" do
+    params = {
+      "email": "whatever@example.com",
+    }
+    post "/api/v1/users", :params => params
+
+    expect(response).to have_http_status(404)
   end
 end
