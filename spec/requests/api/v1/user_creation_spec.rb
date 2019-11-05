5d11ad0e629f48d4e3d88ef36ea9a7d7a3b0a99f
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 13:18:17 -0700

    adds test for endpoint

--- spec/requests/api/v1/user_creation_spec.rb
+++ spec/requests/api/v1/user_creation_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
-describe '' do
-  it "text" do
-
+describe 'Create User object' do
+  it "generates a random API key" do
+    params = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+    post "/api/v1/users", :params => params
+    expect(response).to be_successful
   end
 end
