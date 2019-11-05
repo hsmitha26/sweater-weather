6eb974e4e5229ed636affbf649b717122cfe111e
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 13:52:37 -0700

    user is successfully created on sign-up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.create!(user_params)
+    binding.pry
+    if user.save
+      #render json with api key
+    else
+      #render 404
+    end
+  end
 
+  private
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
