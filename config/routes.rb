bcd0167bf662f5037e9a6a7e4243f9d47779ea2d
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 13:18:32 -0700

    adds new route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#show'
       get '/antipode', to: 'antipode#show'
       get '/backgrounds', to: 'background#show'
+
+      post '/users', to: 'users#create'
     end
   end
 end
