327c0d38a569461510cebdaef286d4a11dc88db3
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 16:50:27 -0700

    adds unit test for SecureRandom

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -5,4 +5,12 @@ describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
+  it "tests SecureRandom" do
+    uuid = "abcdefgh12345"
+    user = User.create(email: "email", password: "password", password_confirmation: 'password')
+    user.uuid = uuid
+    allow(SecureRandom).to receive(:uuid).and_return(uuid)
+    expect(user.create_uuid).to eq(uuid)
+  end
 end
