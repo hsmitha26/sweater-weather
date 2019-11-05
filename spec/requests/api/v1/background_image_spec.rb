1a44d2ed3cccdfed625370b3bbb8e546695ffcd8
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 13:17:46 -0700

    changes name of test to be clearer and adds expectations for response.body

--- spec/requests/api/v1/background_image_spec.rb
+++ spec/requests/api/v1/background_image_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
-describe 'Consume API: ' do
-  it "Unsplash" do
+describe 'Consume API: Unsplash' do
+  it "renders serialized JSON object" do
     location = 'Denver,CO'
     get "/api/v1/backgrounds?location=#{location}"
+
     expect(response).to be_successful
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_response[:data][:attributes]).to have_key(:image_urls)
+    expect((parsed_response[:data][:attributes]).count).to eq(2)
   end
 end
