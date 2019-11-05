e6d287cf9630feca3865a4e21ff01c8fee928126
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-04 17:13:16 -0700

    clears white space; rendered JSON in Postman for Hong Kong

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,11 +1,6 @@
 class Api::V1::BackgroundController < ApplicationController
   def show
     data_prep = BackgroundImageFacade.new(params[:location]).unsplash_data
-binding.pry
     render json: BackgroundImageSerializer.new(data_prep)
-
-    #make a PORO for data prep
-    # pass PORO to serializer and render json
-# binding.pry
   end
 end
