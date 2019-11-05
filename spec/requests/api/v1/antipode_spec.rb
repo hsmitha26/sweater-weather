945227d87b4ac8d7ec5f060db095a5a453ebcb5f
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 10:50:57 -0700

    boosting tests

--- spec/requests/api/v1/antipode_spec.rb
+++ spec/requests/api/v1/antipode_spec.rb
@@ -4,6 +4,12 @@ describe 'Consume APIs: ' do
   it "Amypode, Google Geocode and Dark Sky" do
     location = 'Hong Kong'
     get "/api/v1/antipode?location=#{location}"
+
     expect(response).to be_successful
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    binding.pry
+    expect(parsed_response[:data][:attributes]).to have_key(:)
   end
 end
--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
-describe 'Consume APIs' do
-  it "Google Geocode and Dark Sky" do
+describe 'Consume APIs: Google Geocode and Dark Sky' do
+  it "renders parsed JSON object" do
     location = 'Denver,CO'
     get "/api/v1/forecast?location=#{location}"
+    
     expect(response).to be_successful
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_response[:data][:attributes]).to have_key(:current_weather)
+    expect((parsed_response[:data][:attributes]).count).to eq(4)
   end
 end
