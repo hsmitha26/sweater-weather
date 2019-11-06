e23ff5b30e0c596bb43386b78740e3699e6cef75
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 20:09:15 -0700

    adds service for Google Directions API

---
+++ app/services/google_directions_service.rb
@@ -0,0 +1,27 @@
+class GoogleDirectionsService
+  def initialize(origin, destination)
+    @origin = origin
+    @destination = destination
+  end
+
+  def travel_time
+    # parsed_response = JSON.parse(connection.body, symbolize_names: true)
+    # duration_in_seconds = parsed_response[:routes][0][:legs][0][:duration][:value]
+    ((parsed_response.to_f)/3600).ceil
+  end
+
+  private
+
+  def parsed_response
+    parsed_response = JSON.parse(connection.body, symbolize_names: true)
+    parsed_response[:routes][0][:legs][0][:duration][:value]
+  end
+
+  def connection
+    Faraday.get('https://maps.googleapis.com/maps/api/directions/json?') do |faraday|
+                  faraday.params[:key] = ENV['google_directions_api_key']
+                  faraday.params[:origin] = @origin
+                  faraday.params[:destination] = @destination
+                end
+  end
+end
