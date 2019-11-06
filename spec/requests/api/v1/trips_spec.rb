1cdf8b575b11412aea3f2f3b4c9810126df07247
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 18:44:02 -0700

    adds initial test

---
+++ spec/requests/api/v1/trips_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'User is authenticated using API key ' do
+  it "renders JSON with forecast for travel time/destination" do
+    user = User.create(email: "whatever@example.com", password: "password", password_confirmation: 'password', uuid: "abcdefgh12345")
+    params = {
+      "origin": "Denver,CO",
+      "destination": "Pueblo,CO",
+      "api_key": "abcdefgh12345"
+    }
+    post "/api/v1/road_trips", :params => params
+    expect(response).to be_successful
+  end
+end
