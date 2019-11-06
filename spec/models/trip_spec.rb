8545dae9e9bace71ce2cc94a1698c17cb154a459
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 18:48:58 -0700

    Adds validations and relationships to model test.

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:origin) }
+    it { should validate_presence_of(:destination) }
+  end
+
+  destination 'relationship' do
+    it { should belong_to :user }
+  end
 end
