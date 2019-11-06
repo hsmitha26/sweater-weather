4421acce1e05c61da184e78ed7ba75e56efce38c
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 19:52:16 -0700

    successfully makes api calls and returns forecast in conroller.

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -6,13 +6,26 @@ class Api::V1::TripsController < ApplicationController
       trip = Trip.new(trip_params)
       trip.user_id = set_user.id
       if trip.save
+        origin = trip.origin
+        destination = trip.destination
         #make call to geocode api to get lat/lng for destination
-        location = trip.destination
-        latitude = GeocodeService.new(location).latitude
-        longitude = GeocodeService.new(location).longitude
+        latitude = GeocodeService.new(destination).latitude
+        longitude = GeocodeService.new(destination).longitude
         #make call to directions api to get travel time
+        connection = Faraday.get('https://maps.googleapis.com/maps/api/directions/json?') do |faraday|
+                      faraday.params[:key] = ENV['google_directions_api_key']
+                      faraday.params[:origin] = origin
+                      faraday.params[:destination] = destination
+                    end
+        parsed_response = JSON.parse(connection.body, symbolize_names: true)
+        #convert seconds to hours and round up
+        duration_in_seconds = parsed_response[:routes][0][:legs][0][:duration][:value]
+        duration_in_hours = ((duration_in_seconds.to_f)/3600).ceil
         #make call to dark sky api to get hourly forecast for travel duration
-        binding.pry
+        forecast = DarkSkyService.new(latitude, longitude).response
+        #get hourly forecast for number of hours.
+        forecast_for_trip = forecast[:hourly][:data].first(duration_in_hours)
+        # binding.pry
       else
         render :not_found
       end
@@ -27,11 +40,11 @@ class Api::V1::TripsController < ApplicationController
     params.permit(:origin, :destination)
   end
 
-  def set_user
-    User.find_by(uuid: user_params[:api_key])
-  end
-
   def user_params
     params.permit(:api_key)
+  end
+
+  def set_user
+    User.find_by(uuid: user_params[:api_key])
   end
 end
