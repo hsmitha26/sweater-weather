889bdb68790ac40b1200f4d08a9948859df206e1
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 19:22:51 -0700

    Associate new trip with user.

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -2,22 +2,30 @@ class Api::V1::TripsController < ApplicationController
   before_action :set_user
 
   def create
-    #check api_key is missing first and then proceed
     if set_user
       trip = Trip.new(trip_params)
-      # trip.user_id = user.id
-      binding.pry
+      trip.user_id = set_user.id
+      if trip.save
+        #make call to directions api to get travel time
+        #make call to geocode api to get lat/lng for destination
+        #make call to dark sky api to get hourly forecast for travel duration
+        binding.pry
+      else
+        render :not_found
+      end
+    else
+      render :unauthorized
     end
   end
 
   private
 
-  def set_user
-    User.find_by(uuid: user_params[:api_key])
-  end
-
   def trip_params
     params.permit(:origin, :destination)
+  end
+
+  def set_user
+    User.find_by(uuid: user_params[:api_key])
   end
 
   def user_params
