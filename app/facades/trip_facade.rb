7594c0def9f0d3751d25bc19009a86b44823956b
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 20:08:56 -0700

    moves code lines for Faraday connection and response data into service

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -7,27 +7,30 @@ class TripFacade
     @travel_time = fetch_travel_time
   end
 
+  def fetch_forecast_for_travel_time
+    forecast = DarkSkyService.new(@latitude, @longitude).response
+    forecast[:hourly][:data].first(@travel_time)
+  end
+
+  private
+
   def fetch_latitude
-    GeocodeService.new(@destination).latitude
+    @_google_geocode_latitude ||= GeocodeService.new(@destination).latitude
   end
 
   def fetch_longitude
-    GeocodeService.new(@destination).longitude
+    @_google_geocode_longitude ||= GeocodeService.new(@destination).longitude
   end
 
   def fetch_travel_time
-    connection = Faraday.get('https://maps.googleapis.com/maps/api/directions/json?') do |faraday|
-                  faraday.params[:key] = ENV['google_directions_api_key']
-                  faraday.params[:origin] = @origin
-                  faraday.params[:destination] = @destination
-                end
-    parsed_response = JSON.parse(connection.body, symbolize_names: true)
-    duration_in_seconds = parsed_response[:routes][0][:legs][0][:duration][:value]
-    duration_in_hours = ((duration_in_seconds.to_f)/3600).ceil
-  end
-
-  def fetch_forecast_for_travel_time
-    forecast = DarkSkyService.new(@latitude, @longitude).response
-    forecast_for_trip = forecast[:hourly][:data].first(@travel_time)
+    GoogleDirectionsService.new(@origin, @destination).travel_time
+    # connection = Faraday.get('https://maps.googleapis.com/maps/api/directions/json?') do |faraday|
+    #               faraday.params[:key] = ENV['google_directions_api_key']
+    #               faraday.params[:origin] = @origin
+    #               faraday.params[:destination] = @destination
+    #             end
+    # parsed_response = JSON.parse(connection.body, symbolize_names: true)
+    # duration_in_seconds = parsed_response[:routes][0][:legs][0][:duration][:value]
+    # duration_in_hours = ((duration_in_seconds.to_f)/3600).ceil
   end
 end
