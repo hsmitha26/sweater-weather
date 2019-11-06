5ed68a4289c1483fa2bdcdf493c02e1e4c1fc3b8
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 18:49:10 -0700

    adds relationship test to model test

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -6,6 +6,10 @@ describe User, type: :model do
     it { should validate_presence_of(:password) }
   end
 
+  destination 'relationship' do
+    it { should have_many :trips }
+  end
+
   it "tests SecureRandom" do
     uuid = "abcdefgh12345"
     user = User.create(email: "email", password: "password", password_confirmation: 'password')
