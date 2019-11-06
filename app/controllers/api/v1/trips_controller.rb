f83c727f4ce67293776bd71991addd5fa132e550
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 19:13:59 -0700

    sets before_action

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,12 +1,26 @@
 class Api::V1::TripsController < ApplicationController
+  before_action :set_user
+
   def create
-    trip = Trip.new(trip_params)
-    binding.pry
+    #check api_key is missing first and then proceed
+    if set_user
+      trip = Trip.new(trip_params)
+      # trip.user_id = user.id
+      binding.pry
+    end
   end
 
   private
 
+  def set_user
+    User.find_by(uuid: user_params[:api_key])
+  end
+
   def trip_params
-    params.permit(:origin, :destination, :api_key)
+    params.permit(:origin, :destination)
+  end
+
+  def user_params
+    params.permit(:api_key)
   end
 end
