386bf19132df0e9359e6c025f36afdfa4610a33b
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 18:53:28 -0700

    adds strong params

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::TripsController < ApplicationController
   def create
+    trip = Trip.new(trip_params)
+    binding.pry
+  end
 
+  private
+
+  def trip_params
+    params.permit(:origin, :destination, :api_key)
   end
 end
