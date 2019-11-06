cbfdd4052e8c77a869b6d7bfc44d1c5787a4a15e
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-05 19:59:10 -0700

    creates TripFaçade to make all api calls and format data.

---
+++ app/facades/trip_facade.rb
@@ -0,0 +1,33 @@
+class TripFacade
+  def initialize(trip)
+    @destination = trip.destination
+    @origin = trip.origin
+    @latitude = fetch_latitude
+    @longitude = fetch_longitude
+    @travel_time = fetch_travel_time
+  end
+
+  def fetch_latitude
+    GeocodeService.new(@destination).latitude
+  end
+
+  def fetch_longitude
+    GeocodeService.new(@destination).longitude
+  end
+
+  def fetch_travel_time
+    connection = Faraday.get('https://maps.googleapis.com/maps/api/directions/json?') do |faraday|
+                  faraday.params[:key] = ENV['google_directions_api_key']
+                  faraday.params[:origin] = @origin
+                  faraday.params[:destination] = @destination
+                end
+    parsed_response = JSON.parse(connection.body, symbolize_names: true)
+    duration_in_seconds = parsed_response[:routes][0][:legs][0][:duration][:value]
+    duration_in_hours = ((duration_in_seconds.to_f)/3600).ceil
+  end
+
+  def fetch_forecast_for_travel_time
+    forecast = DarkSkyService.new(@latitude, @longitude).response
+    forecast_for_trip = forecast[:hourly][:data].first(@travel_time)
+  end
+end
